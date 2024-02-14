fee09d1bd4aac440920982c0a174416abcc426ab
Scott Shubert <scottmshubert@gmail.com>
2024-02-14 12:37:57 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import NavBar from './components/NavBar';
+import Product from './components/Product';
+
+function App() {
+	return (
+		<>
+			<NavBar />
+			<Product />
+			{/* <div>
+				Challenge by{' '}
+				<a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+					Frontend Mentor
+				</a>
+				. Coded by <a href='#'>Scott Shubert</a>.
+			</div> */}
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,5 @@
+function NavBar() {
+	return <div> Collections Men Women About Contact</div>;
+}
+
+export default NavBar;
